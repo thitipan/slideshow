69cad7c7430591f9f7b30c70788e40bd43e106b2
thitipan <arm.thitipan@gmail.com>
2015-08-10 11:31:34 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html'); 
+});
+
+app.get('/control', function (req, res) {
+  res.sendFile(__dirname + '/control.html'); 
+});
+
+io.on('connection', function (socket) {
+  socket.on('navigate', function (page) {
+    io.emit('navigate', page);
+  });
+});
